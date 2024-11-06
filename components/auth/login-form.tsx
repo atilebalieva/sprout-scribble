97fa9b7df3af6303671b002715c24e64acb78fc9
atilebalieva <aitilebalieva@gmail.com>
2024-11-06 12:52:59 -0600

    Updated card styles.

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,7 +5,7 @@ import { AuthCard } from "./auth-card";
 export const LoginForm = () => {
   return (
     <AuthCard cardTitle="Welcome back!" backButtonHref="auth/register" backButtonLabel="Create new Account" showSocial>
-      <div>Loginform</div>
+      <div>Login form</div>
     </AuthCard>
   );
 };
--- components/auth/socials.tsx
+++ components/auth/socials.tsx
@@ -2,12 +2,28 @@
 
 import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 export const Socials = () => {
   return (
-    <div>
-      <Button onClick={() => signIn("google", { redirect: false, callbackUrl: "/" })}>Sign in with Google</Button>
-      <Button onClick={() => signIn("github", { redirect: false, callbackUrl: "/" })}>Sign in with Github</Button>
+    <div className="flex flex-col items-ceenter w-full gap-4">
+      <Button
+        variant={"outline"}
+        className="flex gap-4 w-full"
+        onClick={() => signIn("google", { redirect: false, callbackUrl: "/" })}
+      >
+        Sign in with Google
+        <FcGoogle className="w-5 h-5" />
+      </Button>
+      <Button
+        variant={"outline"}
+        className="flex gap-4 w-full"
+        onClick={() => signIn("github", { redirect: false, callbackUrl: "/" })}
+      >
+        Sign in with Github
+        <FaGithub className="w-5 h-5" />
+      </Button>
     </div>
   );
 };
