87d2910ffe75964d3871e1c85fbffc223f91a1e0
atilebalieva <aitilebalieva@gmail.com>
2024-11-28 08:40:23 -0600

    created products component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Products from "@/components/products/products";
+import { db } from "@/server";
+
+export const revalidate = 60 * 60;
+
 export default async function Home() {
-  return <div>Home</div>;
+  const data = await db.query.productVariants.findMany({
+    with: {
+      variantImages: true,
+      variantTags: true,
+      product: true,
+    },
+    orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
+  });
+
+  return (
+    <main className="">
+      <Products variants={data} />
+    </main>
+  );
 }
