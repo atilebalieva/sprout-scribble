0c1c28fc3022d65eeb0c093989950e600876fe01
atilebalieva <aitilebalieva@gmail.com>
2024-12-01 12:10:40 -0600

    aded Reviews components.

--- components/reviews/reviews.tsx
+++ components/reviews/reviews.tsx
@@ -3,6 +3,7 @@ import { db } from "@/server"; /*  */
 import ReviewsForm from "./reviews-form";
 import { desc, eq } from "drizzle-orm";
 import { reviews } from "@/server/schema";
+import Review from "./review";
 /* import ReviewChart from "./review-chart"; */
 
 export default async function Reviews({ productID }: { productID: number }) {
@@ -18,7 +19,7 @@ export default async function Reviews({ productID }: { productID: number }) {
         <div className="flex-1">
           <h2 className="text-2xl font-bold mb-4">Product Reviews</h2>
           <ReviewsForm />
-          {/*           <Review reviews={data} /> */}
+          <Review reviews={data} />
         </div>
         <div className="flex-1 flex flex-col gap-2">{/*        <ReviewChart reviews={data} /> */}</div>
       </div>
